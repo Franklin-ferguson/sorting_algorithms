b2ef24eb14dd016f00a64a788cdaab08af3fda30
root <hayiborfranklin@gmail.com>
2024-03-05 07:43:03 -0800

    hi

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,35 +13,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t prev;
-	size_t next = size;
-	bool is_done = false;
+	size_t i, j;
+    	bool is_sorted = false;
 
+   	if (array == NULL || size < 2)
+        	return;
 
-	if (array == NULL || size < 2)
-	{
-		return;
-	}
-
-	while (is_done == false)
-	{
-		is_done = true;
-		for (prev = 0; prev < next - 1; prev++)
-		{
-			if (array[prev] > array[prev + 1])
-			{
-				int temp;
-			       	int *back, *front;
-
-				back = &array[prev];
-				front = &array[prev + 1];
-				temp = *back;
-				*back = *front;
-				*front = temp;
-				print_array(array, size);
-				is_done = false;
-			}
-		}
-		next--;
-	}
+    	for (i = 0; i < size - 1 && !is_sorted; i++)
+    	{
+        	is_sorted = true;
+        	for (j = 0; j < size - i - 1; j++)
+        	{
+            		if (array[j] > array[j + 1])
+            		{
+                		int temp = array[j];
+                		array[j] = array[j + 1];
+               			array[j + 1] = temp;
+                		print_array(array, size);
+                		is_sorted = false;
+            		}
+        	}
+    	}
 }
